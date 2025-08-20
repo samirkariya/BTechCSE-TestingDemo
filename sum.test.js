93b7581ba3227c07579bd3777c6758f6b9fad336
Samir <samir.kariya@darshan.ac.in>
2025-08-20 11:16:20 +0530

    Fourth Commit changes in sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -6,6 +6,6 @@ expect(add(2,3)).toBe(5);
 });
 
 
-test('subtracts 5 - 3 = 3 (intentional fail)', () => {
-expect(sub(5,3)).toBe(3);
+test('subtracts 5 - 3 = 2 (intentional Pass)', () => {
+expect(sub(5,3)).toBe(2);
 });
